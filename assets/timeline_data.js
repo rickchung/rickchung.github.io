ea98e66d82ef6190ab0ff5e42cbc50b8e9692f5f
Rick Chung <rickjung88@gmail.com>
2021-08-16 12:02:19 -0700

    Adjust the content

--- assets/timeline_data.js
+++ assets/timeline_data.js
@@ -14,15 +14,15 @@ const data_proj = [
 	['2019-05', 'Project', 'Developed a Context-Aware Mobile App for Fall Detection ', 'ASU CSE535 Mobile Computing', 'Course Project', 'Android,Java,ML', ''],
 	['2019-05', 'Project', 'Developed Topic Translation in Augmented Reality', 'ASU CSE578 Data Visualization', 'Course Project', 'Python,C#/Unity,NLP', ''],
 	['2018-12', 'Project', 'Developed AI Agent Based on Deep Q-Learning Network in Pacman', 'ASU CSE571 Artificial Intelligence', 'Featured Project', 'Python,AI,CV', ''],
-	['2018-05', 'Project', 'Developed and Researched a Semi-Supervised Learning Approach for Spectral Waveforms', 'ASU CSE575 Statistical Machine Learning', 'Course Project', 'Python,ML', ''],
+	['2018-05', 'Project', 'Developed and Researched a Semi-Supervised Learning Approach for Spectral Waveforms', 'ASU CSE575 Statistical Machine Learning', 'Course Project', 'Python,ML,Shell', ''],
 	['2018-05', 'Project', 'Modeled the Dynamics of the Opioid Crisis through Twitter Arizona State University', 'ASU CSE576 Natural Language Processing', 'Course Project', 'Python,ML,SVM', ''],
 	['2018-12', 'Project', 'Developed a Programmable, 3D-printed, Cyber-physical Robot', 'ASU CSE564 Software Design', 'Course Project', 'C/C++,3D Printer', ''],
 	['2020-12', 'Project', 'Developed a Web-based Augmented Reality Template', 'ASU, Tempe, AZ', 'Research Project', 'JavaScript,HTML/CSS,AR', ''],
 	['2020-10', 'Project', 'Developed a Mobile SEIR Data Visualization', 'ASU, Tempe, AZ', 'Side Project', 'Python,DataVis,SEIR', ''],
 	['2016-03', 'Project', 'Designed a Tutorial - Introduction to Python Programming', 'NSYSU, TW', 'Side Project', 'Python,Tutorial', ''],
-	['2015-08', 'Project', 'Engineered a Personal Emotion Assistant based on Biosensors', 'NSYSU, TW', 'Featured Project', 'Android,Django,Arduino', ''],
+	['2015-08', 'Project', 'Engineered a Personal Emotion Assistant based on Biosensors', 'NSYSU, TW', 'Featured Project', 'Android,Django,Arduino,Shell', ''],
 	['2016-01', 'Project', 'Conducted a Trend Analysis on a Discussion Forum', 'NSYSU E-Commerce', 'Course Project', 'Python,Scrapy', ''],
-	['2013-05', 'Project', 'Developed a Game in Python - Space Invador', 'NSYSU Principles of Computing', 'Course Project', 'Python,PyGame', ''],
+	['2013-05', 'Project', 'Developed a Game in Python - Space Invador', 'NSYSU Principles of Computing', 'Course Project', 'Python,PyGame,Shell', ''],
 	['2014-12', 'Project', 'Engineered a C Shell for Operating Systems', 'NSYSU Operating Systems', 'Course Project', 'C/C++,Linux,Shell', ''],
 	['2014-03', 'Project', 'Developed an Online Lottery Drawer', 'NSYSU Database Management', 'Side Project', 'HTML/CSS,JavaScript', ''],
 	['2014-09', 'Project', 'Developed a Course Selection System', 'NSYSU, TW', 'Side Project', 'HTML/CSS,PHP,JavaScript,SQL', ''],
@@ -49,7 +49,7 @@ const data_pub = [
 	['2021', 'Publication', 'Interactive CovidCampus Simulation Game: Genesis, Design, and Outcomes', 'Front. Commun', 'Collaboration', '', '']
 ];
 const data_work = [
-	['2017-08', 'Work', 'Researched and developed a content recommendar by TensorFlow', 'Data Science Intern at Dcard, Taipei, Taiwan', 'Paid Work', 'Python,TensorFlow,Recommendation', ''],
+	['2017-08', 'Work', 'Researched and developed a content recommendar by TensorFlow', 'Data Science Intern at Dcard, Taipei, Taiwan', 'Paid Work', 'Python,TensorFlow,Recommendation,Shell', ''],
 	['2017-02', 'Work', 'Developed an Online 3D Model Viewer', 'TW', 'Paid Work', 'JavaScript,HTML/CSS,3D Model', ''],
 	['2017', 'Work', 'TA - CSE572 Data Mining', 'ASU, Tempe, AZ', 'Paid Work', '', ''],
 	['2017', 'Work', 'TA - CSE110 Principles of Programming (Java)', 'ASU, Tempe, AZ', 'Paid Work', 'Java', ''],
@@ -59,7 +59,7 @@ const data_work = [
 	['2019', 'Work', 'TA - CPI101 Introduction to Informatics', 'ASU, Tempe, AZ', 'Paid Work', 'HTML/CSS,JavaScript,SQL', ''],
 	['2020', 'Work', 'TA - CSE110 Principles of Programming (Java)', 'ASU, Tempe, AZ', 'Paid Work', 'Java', ''],
 	['2020', 'Work', 'TA/Instructor - CSE578 Data Visualization', 'ASU, Tempe, AZ', 'Paid Work', 'Python', ''],
-	['2021', 'Work', 'TA - CSE205 Object-Oriented Program & Data (Java)', 'ASU, Tempe, AZ', 'Paid Work', 'Java', ''],
+	['2021', 'Work', 'TA - CSE205 Object-Oriented Program & Data (Java)', 'ASU, Tempe, AZ', 'Paid Work', 'Java,Shell,GUI', ''],
 	['2021', 'Work', 'TA - CSE579 Knowledge Representation and Reasoning', 'ASU, Tempe, AZ', 'Paid Work', 'Clingo,AI,ASP', ''],
 	['2021', 'Work', 'TA - CSE578 Data Visualization (Mandarin/English Bilingual)', 'ASU, Tempe, AZ', 'Paid Work', 'Python', '']
 ];
