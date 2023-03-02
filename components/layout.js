072c3086aca667cfa9b8bc1f6179b3f9c08e38d2
Rick <rickjung88@gmail.com>
2023-03-02 11:53:33 -0700

    Convert to tsx

---
+++ components/layout.js
@@ -0,0 +1,11 @@
+/**
+ * Global layout file. See https://nextjs.org/learn/basics/assets-metadata-css/layout-component
+ */
+
+const Layout = ({ children }) => {
+    return (
+        <div>{children}</div>
+    );
+}
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button'
+
+const MyNextWebsite = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+};
+
+export default MyNextWebsite;
