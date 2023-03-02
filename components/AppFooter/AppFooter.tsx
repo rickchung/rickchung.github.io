8054bdb60131ec040c4181c264f5c5edf03f771a
Rick <rickjung88@gmail.com>
2023-03-02 15:17:21 -0700

    Refactor footer content

--- components/AppFooter/AppFooter.tsx
+++ components/AppFooter/AppFooter.tsx
@@ -1,28 +1,29 @@
-import { useTheme } from "@emotion/react";
 import { Box, Divider, Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 
-const AppFooter = () => {
-  const theme = useTheme();
+const footerContent = [
+  { title: "...", content: "\"But where, where was this self, this innermost part, this ultimate part?\"", size: 4 },
+  { title: "...", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione atque iure voluptate quia deserunt commodi, aut quaerat consequuntur alias in numquam consectetur nulla nostrum. Necessitatibus culpa modi nemo dicta placeat.", size: 8 },
+];
 
+const AppFooter = () => {
   return (
     <>
       <Box pt={4} pb={4}><Divider /></Box>
       <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <Container>
-            <Typography variant="body1" component="p">
-            "But where, where was this self, this innermost part, this ultimate part?"
-            </Typography>
-          </Container>
-        </Grid>
-        <Grid item xs={8}>
-          <Container>
-            <Typography variant="body1" component="p">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione atque iure voluptate quia deserunt commodi, aut quaerat consequuntur alias in numquam consectetur nulla nostrum. Necessitatibus culpa modi nemo dicta placeat.
-            </Typography>
-          </Container>
-        </Grid>
+        {footerContent.map(({ title, content, size }: {
+          title?: string,
+          content: string,
+          size: number,
+        }) => (
+          <Grid item xs={size}>
+            <Container>
+              <Typography variant="body1" component="p">
+                {content}
+              </Typography>
+            </Container>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
