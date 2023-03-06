5096df41d8b28197abeca82b4f8d4228c17bfc6a
Rick <rickjung88@gmail.com>
2023-03-06 13:05:33 -0700

    Docs

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,4 @@
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-d-ts.html
 
+// Treat .md files as modules
 declare module '*.md';
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 const nextConfig = {
     webpack: (config, options) => {
+        // Make Webpack to load a .md file as a string by import
         config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
         return config;
     }
