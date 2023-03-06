5c7bb114984c39430b84aa6ca5533df05ec79f83
Rick <rickjung88@gmail.com>
2023-03-06 15:01:42 -0700

    Load post content

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,33 @@
+import { readFileSync } from "fs";
 import { glob } from "glob";
+import matter from "gray-matter";
 import { join } from "path";
 
 const postRepo = join(process.cwd(), "sitedata", "posts", "*.md");
+
 export async function getAllPostFullPaths() {
     return await glob(postRepo);
 };
 
+export async function getAllPosts(fields = ['title', 'content']) {
+    const paths = await getAllPostFullPaths();
+
+    type Item = {
+        [key: string]: string
+    }
+
+    const items: Item[] = paths.map((i) => {
+        const { data, content } = matter(readFileSync(i, 'utf-8'));
+        const item: Item = {};
+        for (const f of fields) {
+            if (f === 'content') {
+                item[f] = content;
+            } else if (typeof data[f] !== 'undefined') {
+                item[f] = data[f];
+            }
+        }
+        return item;
+    });
+
+    return items;
+};
--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,9 +1,8 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import path from "path";
 
-import { getAllPostFullPaths } from "../lib/api";
 import Layout from "../components/Layout";
+import { getAllPosts } from "../lib/api";
 
 type Props = {
   posts: any[]
@@ -17,7 +16,9 @@ export default function Archive({ posts }: Props) {
     <Layout>
       <Box pt={2}>
         <Typography variant="h5">All Posts</Typography>
-        {posts}
+        <ul>
+          {posts.map((p, i) => (<li key={i}>{p.title}</li>))}
+        </ul>
       </Box>
     </Layout>
   )
@@ -27,8 +28,6 @@ export default function Archive({ posts }: Props) {
  * Run at the build time to retrieve all post files from a folder. Ref: https://github.com/vercel/next.js/tree/canary/examples/blog-starter
  */
 export async function getStaticProps() {
-  const postFullPaths = await getAllPostFullPaths();
-  const postFnames = postFullPaths.map((x) => path.basename(x));
-  console.log(postFnames);
-  return { props: { postFnames } };
+  const posts = await getAllPosts();
+  return { props: { posts } };
 };
