1aabe2c429dbcc0fc4804c69ead1682ad36ed18c
Rick <rickjung88@gmail.com>
2023-03-06 20:57:12 -0700

    Add a page Archive

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,26 +1,28 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import Head from "next/head";
 
 import Layout from "../components/Layout";
 import { getAllPosts } from "../lib/api";
 
-type Props = {
-  posts: any[]
-};
-
 /**
  * Display an archive list of all website posts. 
  */
-export default function Archive({ posts }: Props) {
+export default function Archive({ posts }: { posts: any[] }) {
   return (
-    <Layout>
-      <Box pt={2}>
-        <Typography variant="h5">All Posts</Typography>
-        <ul>
-          {posts.map((p, i) => (<li key={i}>{p.title}</li>))}
-        </ul>
-      </Box>
-    </Layout>
+    <>
+      <Head>
+        <title>Post Archive - Yet Another CYC</title>
+      </Head>
+      <Layout>
+        <Box pt={2}>
+          <Typography variant="h5">All Posts</Typography>
+          <ul>
+            {posts.map((p, i) => (<li key={i}>{p.title}</li>))}
+          </ul>
+        </Box>
+      </Layout>
+    </>
   )
 };
 
